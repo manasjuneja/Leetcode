d553f2018648b8e8a259582c0bfa7643bea55349
manasjuneja <junejamans@proton.me>
2023-02-04 09:55:15 +0530

    202) Happy Number

--- 202-HappyNumber/solution1.py
+++ 202-HappyNumber/solution1.py
@@ -1,8 +1,21 @@
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        a = []
-        for i in nums:
-            a.append(i*i)
-        a.sort()
-        return a
-        
+    def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+        elif n < 6:
+            return False
+            
+        while (n>6):
+            sum = 0
+            x = str(n)
+
+            for i in x:
+                sum = sum + int(i)**2
+
+            if sum == 1:
+                return True
+            elif sum < 6:
+                return False
+            else:
+                n = sum
+
--- solution1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        a = []
-        for i in nums:
-            a.append(i*i)
-        a.sort()
-        return a
-        
