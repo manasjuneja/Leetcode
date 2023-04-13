ce1bc7b9e7d67c4475b28d5d8a8b3f41f4e69883
manasjuneja <junejamans@proton.me>
2023-04-13 14:35:14 +0530

    1470) Shuffle The Array using Python

---
+++ 1470-ShuffleTheArray/README.md
@@ -0,0 +1,6 @@
+# Complexity Ananlysis
+
+## Solution 1
+
+Time Complexity: O(n) : where n is the number of elements in the given array.
+Space Complexity: O(n) : where n is the number of elements in the given array.
---
+++ 1470-ShuffleTheArray/solution1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        x = []
+
+        for i in range(n):
+            x.append(nums[i])
+            x.append(nums[n+i])
+
+        return x
